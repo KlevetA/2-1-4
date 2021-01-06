0a4561ae199b0a3527c9625137b014f6be35fb75
user <tolbin.ag@gmail.com>
2021-01-06 17:48:01 +0300

    finish

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -2,6 +2,8 @@ package koschei.config;
 
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import koschei.models.Needle7;
+import koschei.models.Deth8;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,4 +16,9 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+   // @Bean
+    //public static Needle7 getNeedle(Deth8 Deth){
+    //    return new Needle7(Deth);
+   // }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+
+    Needle7 needle;
+
+    @Autowired
+    public void Needle7(Needle7 Needle7){
+        this.needle = Needle7;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString() ;
     }
 }
